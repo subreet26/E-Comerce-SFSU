d17b6069aafc0c67d33b97799d544fa9f8aa64e3
michalkrupa <mkrupa@sfsu.edu>
2026-04-11 21:26:21 -0700

    [hotfix] Set DEBUG to False in settings.py
    Changed DEBUG setting to False for production.

--- application/config/settings.py
+++ application/config/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p3^&n&nd(x%jkff32*h(f+v_%%gq41pzq9!4p0+m19w8lgz+3d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
 	"3.135.198.114",
