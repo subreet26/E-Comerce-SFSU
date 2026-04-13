734e60f649b7919a386e5305ede59a27eb5b2f08
subreet26 <123537183+subreet26@users.noreply.github.com>
2026-04-12 22:03:50 -0700

    HOTFIX Fetch categories from database instead of constant

--- application/marketplace/context_processors.py
+++ application/marketplace/context_processors.py
@@ -3,6 +3,6 @@ from backend.models import Category
 
 def marketplace_globals(request):
     return {
-        "categories": CATEGORIES,
+        "categories": Category.objects.all().order_by("category_name"),
         "query": (request.GET.get("q") or request.GET.get("query") or "").strip(),
     }
